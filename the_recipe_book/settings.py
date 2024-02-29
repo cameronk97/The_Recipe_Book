0dbc1d7f5258e1fe6799d64a98107500756485c0
Cameron Kirby <cameronlrk97@gmail.com>
2024-02-29 15:37:31 +0000

    Remove unnecessary protocol from Allowed Hosts

--- the_recipe_book/settings.py
+++ the_recipe_book/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEVELOPMENT', '').lower() == 'true'
 
-ALLOWED_HOSTS = ["the-recipe-book.herokuapp.com", "https://the-recipe-book-2bdd46ce4dd3.herokuapp.com/", "127.0.0.1"]
+ALLOWED_HOSTS = ["the-recipe-book.herokuapp.com", "the-recipe-book-2bdd46ce4dd3.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
