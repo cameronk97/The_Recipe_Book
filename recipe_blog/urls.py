75ad1d260c3e5af579173150f5ef049672778a00
Cameron Kirby <cameronlrk97@gmail.com>
2024-03-01 23:19:07 +0000

    Add URL patterns for basic views

---
+++ recipe_blog/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('recipe/', views.recipe_page, name='recipe_page'),
+    path('post/', views.post_recipe, name='post_recipe'),
+    path('edit/', views.edit_recipe, name='edit_recipe'),
+    path('profile/', views.profile, name='profile'),
+    path('saved/', views.saved_recipes, name='saved_recipes'),
+]
