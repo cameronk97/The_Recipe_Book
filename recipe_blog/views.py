cd186bc87ff972e83ecf2f61f5c9235f5ea7ff31
Cameron Kirby <cameronlrk97@gmail.com>
2024-03-01 23:15:12 +0000

    Add initial view functions to render templates

--- recipe_blog/views.py
+++ recipe_blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def recipe_page(request):
+    return render(request, 'recipe_page.html')
+
+
+def post_recipe(request):
+    return render(request, 'post_recipe.html')
+
+
+def edit_recipe(request):
+    return render(request, 'edit_recipe.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def saved_recipes(request):
+    return render(request, 'saved_recipes.html')
