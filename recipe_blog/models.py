5e92f6b328a45871930fd136b924356e9bb61a71
Cameron Kirby <cameronlrk97@gmail.com>
2024-03-02 04:37:12 +0000

    Import default models and create ERD-based models

--- recipe_blog/models.py
+++ recipe_blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    ingredients = models.TextField
+    instructions = models.TextField
+    image = CloudinaryField('image', default='placeholder')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts")
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    saves = models.ManyToManyField(User, related_name='recipe_saves', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return str(self.id)
+
+    def number_of_saves(self):
+        return self.saves.count()
+
+
+class SavedRecipes(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="saved_recipes")
+    user = models.ManyToManyField(User, blank=True)
