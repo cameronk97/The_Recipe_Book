f4d2f71c0faa5345182b63f8e33b666284517bbf
Cameron Kirby <cameronlrk97@gmail.com>
2024-03-02 17:56:30 +0000

    Update the RecipeAdmin class and prepopulate Slug

--- recipe_blog/admin.py
+++ recipe_blog/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Recipe
 
 # Register your models here.
-admin.site.register(Recipe)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created_on', 'updated_on')
