005e6aa1a7a5117139a600bf09ac5e203a164817
Cameron Kirby <cameronlrk97@gmail.com>
2024-07-07 23:23:07 +0100

    Create URL pattern to display recipes on homepage

--- recipe_blog/urls.py
+++ recipe_blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', views.RecipeList.as_view(), name='home'),
     path('recipe/', views.recipe_page, name='recipe_page'),
     path('post/', views.post_recipe, name='post_recipe'),
     path('edit/', views.edit_recipe, name='edit_recipe'),
